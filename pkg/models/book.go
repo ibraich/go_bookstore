b5c6c64523af9c43fcdfe332aaf1000e0caea9ac
ibrahim erdogan <ibo9819@gmail.com>
2024-04-10 12:56:17 +0200

    book.go/ models are set

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,1 +1,22 @@
 package models
+
+import (
+	"github.com/ibraich/go_bookstore/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDb()
+	db.AutoMigrate(&Book{})
+
+}
