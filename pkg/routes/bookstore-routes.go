383361387d74bc8db97568d514658d1c2ff95393
ibrahim erdogan <ibo9819@gmail.com>
2024-04-10 11:56:48 +0200

    routes are put

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/ibraich/go_bookstore/pkg/controllers"
+)
+
+func RegisterBookStoreRoutes(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookid}", controllers.GetBookById).Methods("Get")
+	router.HandleFunc("/book/{bookid}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookid}", controllers.DeleteBook).Methods("DELETE")
+}
