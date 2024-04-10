d100996dbb2ce4201229a1ebf90e413aed65c173
ibrahim erdogan <ibo9819@gmail.com>
2024-04-10 17:34:58 +0200

    main. go is set

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/ibraich/go_bookstore/pkg/routes"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9001", r))
+}
